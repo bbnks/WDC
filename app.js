2833beb095e431360f9bbc6c640f54d0978f3ed0
Brenden Banks <banks334@gmail.com>
2025-07-27 17:31:47 -0400

    Fix JSON parsing and update schema to match CMS API

--- app.js
+++ app.js
@@ -1,42 +1,59 @@
 (function () {
   var myConnector = tableau.makeConnector();
 
-  myConnector.getSchema = function (schemaCallback) {
-    var cols = [
-      { id: "provider", alias: "Provider Name", dataType: tableau.dataTypeEnum.string },
-      { id: "state", alias: "State Code", dataType: tableau.dataTypeEnum.string },
-      { id: "measure", alias: "Measure Code", dataType: tableau.dataTypeEnum.string },
-      { id: "score", alias: "Score", dataType: tableau.dataTypeEnum.string },
-      { id: "sample", alias: "Sample", dataType: tableau.dataTypeEnum.int }
+  myConnector.getSchema = function(schemaCallback) {
+    const cols = [
+        { id: "rpt_rec_num", alias: "Report Number", dataType: tableau.dataTypeEnum.string },
+        { id: "provider_ccn", alias: "Provider CCN", dataType: tableau.dataTypeEnum.string },
+        { id: "name", alias: "Hospital Name", dataType: tableau.dataTypeEnum.string },
+        { id: "address", alias: "Street Address", dataType: tableau.dataTypeEnum.string },
+        { id: "city", alias: "City", dataType: tableau.dataTypeEnum.string },
+        { id: "state", alias: "State Code", dataType: tableau.dataTypeEnum.string },
+        { id: "zip", alias: "Zip Code", dataType: tableau.dataTypeEnum.string },
+        { id: "facility_type", alias: "CCN Facility Type", dataType: tableau.dataTypeEnum.string },
+        { id: "ownership", alias: "Type of Control", dataType: tableau.dataTypeEnum.string },
+        { id: "fy_start", alias: "Fiscal Year Begin", dataType: tableau.dataTypeEnum.date },
+        { id: "fy_end", alias: "Fiscal Year End", dataType: tableau.dataTypeEnum.date }
     ];
 
-    var tableSchema = {
-      id: "cms_quality",
-      alias: "CMS Quality Measures (NY)",
-      columns: cols
+    const tableSchema = {
+        id: "cms_data",
+        alias: "CMS Hospital Dataset",
+        columns: cols
     };
 
     schemaCallback([tableSchema]);
-  };
+};
 
-  myConnector.getData = function (table, doneCallback) {
-    var url = "https://data.cms.gov/data-api/v1/dataset/44060663-47d8-4ced-a115-b53b4c270acb/data?state_cd=NY&limit=10000";
+
+myConnector.getData = function(table, doneCallback) {
+    const url = "https://data.cms.gov/data-api/v1/dataset/44060663-47d8-4ced-a115-b53b4c270acb/data?state_cd=NC";
 
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        var tableData = data.map(item => ({
-          provider: item["Provider Name"],
-          state: item["State Code"],
-          measure: item["Measure Code"],
-          score: item["Score"],
-          sample: parseInt(item["Sample"]) || 0
-        }));
+        .then(response => response.json())
+        .then(data => {
+            const tableData = [];
 
-        table.appendRows(tableData);
-        doneCallback();
-      });
-  };
+            data.forEach(item => {
+                tableData.push({
+                    rpt_rec_num: item["rpt_rec_num"],
+                    provider_ccn: item["Provider CCN"],
+                    name: item["Hospital Name"],
+                    address: item["Street Address"],
+                    city: item["City"],
+                    state: item["State Code"],
+                    zip: item["Zip Code"],
+                    facility_type: item["CCN Facility Type"],
+                    ownership: item["Type of Control"],
+                    fy_start: item["Fiscal Year Begin Date"],
+                    fy_end: item["Fiscal Year End Date"]
+                });
+            });
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
+};
 
   tableau.registerConnector(myConnector);
 
